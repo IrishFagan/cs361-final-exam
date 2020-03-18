c0b34124be3e0e20a180550ba4987deed75b807e
Irish Fagan <IrishFagan@live.com>
2020-03-18 12:07:03 -0700

    Fix attr_accessor syntax References #1

--- mountain_bike.rb
+++ mountain_bike.rb
@@ -2,7 +2,7 @@ require_relative 'roll_pack'
 
 class MountainBike
 
-  attr_accessor :luggage :weekly_price :daily_price :hourly_price
+  attr_accessor :luggage, :weekly_price
 
   def initialize(luggage = RollPack.new, weekly_price = 90, daily_price = 25, hourly_price = 10)
     @luggage = luggage
